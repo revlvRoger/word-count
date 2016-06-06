819c733b71445d261b1f18569677a24420606114
revlvRoger <revlv.roger@gmail.com>
2016-06-06 17:47:36 +0800

    last commit

--- index.php
+++ index.php
@@ -8,6 +8,42 @@
 <body>
 <div class="container">
     <div class="wrapper">
+    <?php
+if(isset($_POST['Submit'])){
+    // loop through the uploaded files
+    foreach ($_FILES as $key => $value){
+        $image_tmp = $value['tmp_name'];
+        $image = $value['name'];
+        $image_file = "{$UPLOADDIR}{$image}";
+
+        // move the file to the permanent location
+        if(move_uploaded_file($image_tmp,$image_file)){
+            echo <<<HEREDOC
+    <img src="{$image_file}" alt="file not found" /></br>
+</div>
+HEREDOC;
+        }
+        else{
+            echo "<h1>image file upload failed, image too big after compression</h1>";
+        }
+    }
+}
+else{
+    ?>
+    <form name='baccs' method='post' enctype='multipart/form-data' action=''>
+    <table>
+    <tr>
+        <td><input type='file' name='image'></td>
+    </tr>
+    <tr>
+        <td><input name='Submit' type='submit' value='Upload image'></td>
+    </tr>
+</table>
+</form>
+<?php
+}
+?>
+        <h1>Top 20 Words</h1>
         <?php
         $myFile = file_get_contents('alice.txt');
         $strlower = strtolower($myFile);
@@ -15,12 +51,14 @@
         echo "<pre>";
         arsort($words);
         print_r(array_slice($words, 0, 20));
+        echo "Counting words";
         ksort($words);
         echo "<pre>";
         print_r($words);
         ?>
     </div>
     <table border="2px">
+        <h1>CSV File</h1>
         <?php
             $fileCsv = fopen("todo.csv", "r");
             while (($line = fgetcsv($fileCsv)) !== false) {
@@ -33,6 +71,7 @@
             fclose($f);
             ?>
 </table>
+
 </div>
 </body>
 </html>
